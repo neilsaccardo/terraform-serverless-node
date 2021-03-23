dd03d7ab896702f9627c17ef48f7ce71ebbaf898
Neil <neil.saccardo@gmail.com>
2021-03-23 20:55:40 +0000

    * Add lambda deployment * Add api gateway

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -14,7 +14,7 @@ resource "aws_lambda_function" "example" {
    function_name = "ServerlessExample"
 
    # The bucket name as created earlier with "aws s3api create-bucket"
-   s3_bucket = "terraform-serverless-example"
+   s3_bucket = "terraform-serverless-example-neiltest"
    s3_key    = "v1.0.0/example.zip"
 
    # "main" is the filename within the zip file (main.js) and "handler"
@@ -49,3 +49,63 @@ EOF
 
 }
 
+resource "aws_api_gateway_resource" "proxy" {
+   rest_api_id = aws_api_gateway_rest_api.example.id
+   parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+   path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+   rest_api_id   = aws_api_gateway_rest_api.example.id
+   resource_id   = aws_api_gateway_resource.proxy.id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+   rest_api_id = aws_api_gateway_rest_api.example.id
+   resource_id = aws_api_gateway_method.proxy.resource_id
+   http_method = aws_api_gateway_method.proxy.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.example.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+   rest_api_id   = aws_api_gateway_rest_api.example.id
+   resource_id   = aws_api_gateway_rest_api.example.root_resource_id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+   rest_api_id = aws_api_gateway_rest_api.example.id
+   resource_id = aws_api_gateway_method.proxy_root.resource_id
+   http_method = aws_api_gateway_method.proxy_root.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.example.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "example" {
+   depends_on = [
+     aws_api_gateway_integration.lambda,
+     aws_api_gateway_integration.lambda_root,
+   ]
+
+   rest_api_id = aws_api_gateway_rest_api.example.id
+   stage_name  = "test"
+}
+
+resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.example.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   # The "/*/*" portion grants access from any method on any resource
+   # within the API Gateway REST API.
+   source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+}
